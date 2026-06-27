5ecaa5469ec057da471d82aa00cc37259d7695de
Arjun Singh <235448300+arjuncodelab@users.noreply.github.com>
2026-06-27 16:55:42 +0530

    Update

--- 02_data_etl.sql
+++ 02_data_etl.sql
@@ -1,29 +1,46 @@
-INSERT INTO countries (country,
- development_level,
+INSERT INTO device_access (device_access)
+SELECT device_access FROM mixed GROUP BY device_access ORDER BY device_access;
+
+INSERT INTO family_income (family_income)
+SELECT family_income_level FROM mixed GROUP BY family_income_level ORDER BY family_income_level;
+
+INSERT INTO study_field (field_of_study)
+SELECT field_of_study FROM mixed GROUP BY field_of_study ORDER BY field_of_study;
+
+INSERT INTO education_level (education_level)
+SELECT education_level FROM mixed GROUP BY education_level ORDER BY education_level;
+
+INSERT INTO development(development_level)
+SELECT development_level FROM mixed GROUP BY development_level ORDER BY development_level;
+
+
+INSERT INTO countries (country, development_id,
  poverty_rate_percent,
  internet_infrastructure_index,
  average_internet_speed_mbps)
-SELECT 
- country,
- development_level,
- poverty_rate_percent,
- internet_infrastructure_index,
- average_internet_speed_mbps
- FROM mixed GROUP BY country order by country;
+SELECT
+ m.country,
+ d.development_id,
+ m.poverty_rate_percent,
+ m.internet_infrastructure_index,
+ m.average_internet_speed_mbps
+ FROM mixed m
+ JOIN development d ON m.development_level = d.development_level
+ GROUP BY country order by country;
 
 
 INSERT INTO students (
-    country_id,
-    country,
     student_id,
+    country_id,
+    development_id,
     age,
     gender,
     urban_rural,
-    family_income_level,
-    device_access,
+    family_income_id,
+    device_id,
     internet_access_hours,
-    education_level,
-    field_of_study,
+    education_id,
+    study_field_id,
     academic_motivation,
     online_learning_hours,
     social_media_hours,
@@ -48,8 +65,8 @@ INSERT INTO students (
     stress_level,
     anxiety_score,
     depression_score,
-    ads_viewed_per_day,
     ads_clicked_per_week,
+    ads_viewed_per_day,
     impulse_purchase_score,
     digital_spending_per_month,
     cyberbullying_exposure,
@@ -59,16 +76,18 @@ INSERT INTO students (
     academic_risk_score,
     financial_risk_score
 )
-SELECT c.country_id, c.country,
+SELECT
     m.student_id,
+    c.country_id,
+    d.development_id,
     m.age,
     m.gender,
     m.urban_rural,
-    m.family_income_level,
-    m.device_access,
+    f.family_income_id,
+    da.device_id,
     m.internet_access_hours,
-    m.education_level,
-    m.field_of_study,
+    e.education_id,
+    s.study_field_id,
     m.academic_motivation,
     m.online_learning_hours,
     m.social_media_hours,
@@ -93,8 +112,8 @@ SELECT c.country_id, c.country,
     m.stress_level,
     m.anxiety_score,
     m.depression_score,
-    m.ads_viewed_per_day,
     m.ads_clicked_per_week,
+    m.ads_viewed_per_day,
     m.impulse_purchase_score,
     m.digital_spending_per_month,
     m.cyberbullying_exposure,
@@ -104,8 +123,12 @@ SELECT c.country_id, c.country,
     m.academic_risk_score,
     m.financial_risk_score
 FROM mixed m
-JOIN countries c ON m.country = c.country;
-
-ALTER TABLE students
-DROP COLUMN country;
+JOIN countries c ON m.country = c.country
+JOIN development d ON m.development_level = d.development_level
+JOIN education_level e ON m.education_level = e.education_level
+JOIN family_income f ON m.family_income_level = f.family_income
+JOIN study_field s ON m.field_of_study = s.field_of_study
+JOIN device_access da ON m.device_access = da.device_access
+ORDER BY m.student_id
+;
 
